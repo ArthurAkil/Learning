929c40e80efefb70265048f18ce88b9b1e8b0cdc
Arthur A <161647016+ArthurAkil@users.noreply.github.com>
2024-05-15 15:26:37 -0300

    game_jokenpô.py

--- game_jokenpô.py
+++ game_jokenpô.py
@@ -1,45 +1,54 @@
-def jokenpô():
-    from random import choice
+def jokenpo():
+    from random import randint
 
-    pedra = 'pedra'
-    papel = 'papel'
-    tesoura = 'tesoura'
+    lista = ['Pedra', 'Papel', 'Tesoura']
+    numero_pc = randint(0, 2)
+    escolha_pc = lista[numero_pc]
     vitorias = 0
     derrotas = 0
+    empates = 0
     while True:
-        print('Deseja jogar ou continuar jogando Jokenpô? 1-Sim 2-Não')
-        mediador = int(input())
-        adversario = ''
-        if mediador == 1:
-            entrada = input('Faça uma escolhe entre Pedra, Papel ou Tesoura: ').lower()
-            adversario = choice([pedra, papel, tesoura])
-            if entrada == 'pedra' and adversario == papel:
-                print(f'Você escolheu {entrada} e o adversário escolheu {adversario}. Você perdeu.')
+        print('-'* 70)
+        print('Bem vindo ao Jokenpô!')
+        print('''Escolha sua opção:
+[0] = Pedra
+[1] = Papel
+[2] = Tesoura
+[3] = Sair do jogo''')
+        numero_usuario = int(input())
+        if numero_usuario == 3:
+            return f'Durante o seu tempo de jogo você conseguiu {empates} empates, {vitorias} vitorias, {derrotas} derrotas.'
+        escolha_usario = lista[numero_usuario]
+        if numero_usuario == 0:
+            print(f'Você escolheu {(lista[numero_usuario]).upper()}', end='')
+            if numero_pc == 0:
+                print(f' e o computador escolheu {(lista[numero_pc]).upper()}. EMPATE!')
+                empates += 1
+            elif numero_pc == 1:
+                print(f' e o computador escolheu {(lista[numero_pc]).upper()}. O computador ganhou! ')
                 derrotas += 1
-            elif entrada == 'pedra' and adversario == tesoura:
-                print(f'Você escolheu {entrada} e o adversário escolheu {adversario}. Você ganhou.')
+            elif numero_pc == 2:
+                print(f' e o computador escolheu {(lista[numero_pc]).upper()}. Você ganhou!')
                 vitorias += 1
-            elif entrada == 'pedra' and adversario == pedra:
-                print(f'Você escolheu {entrada} e o adversário escolheu {adversario}. Empate.')
-
-            elif entrada == 'papel' and adversario == tesoura:
-                print(f'Você escolheu {entrada} e o adversário escolheu {adversario}. Você perdeu.')
+        elif numero_usuario == 1:
+            print(f'Você escolheu {lista[numero_usuario]}', end='')
+            if numero_pc == 0:
+                print(f' e o computador escolheu {lista[numero_pc]}. Você ganhou!')
+                vitorias += 1
+            elif numero_pc == 1:
+                print(f' e o computador escolheu {lista[numero_pc]}. EMPATE!')
+                empates += 1
+            elif numero_pc == 2:
+                print(f' e o computador escolheu {lista[numero_pc]}. O computador ganhou!')
                 derrotas += 1
-            elif entrada == 'papel' and adversario == pedra:
-                print(f'Você escolheu {entrada} e o adversário escolheu {adversario}. Você ganhou.')
-                vitorias += 1
-            elif entrada == 'papel' and adversario == papel:
-                print(f'Você escolheu {entrada} e o adversário escolheu {adversario}. Empate.')
-
-            elif entrada == 'tesoura' and adversario == tesoura:
-                print(f'Você escolheu {entrada} e o adversário escolheu {adversario}. Empate.')
-            elif entrada == 'tesoura' and adversario == pedra:
-                print(f'Você escolheu {entrada} e o adversário escolheu {adversario}. Você perdeu.')
+        elif numero_usuario == 2:
+            print(f'Você escolheu {lista[numero_usuario]}', end='')
+            if numero_pc == 0:
+                print(f' e o computador escolheu {lista[numero_pc]}. O computador ganhou!')
                 derrotas += 1
-            elif entrada == 'tesoura' and adversario == papel:
-                print(f'Você escolheu {entrada} e o adversário escolheu {adversario}. Você ganhou.')
+            elif numero_pc == 1:
+                print(f' e o computador escolheu {lista[numero_pc]}. Você ganhou!')
                 vitorias += 1
-        elif mediador == 2:
-            return f'A contagem de suas vitorias contra o computador foram {vitorias} e suas derrotas foram {derrotas}.'
-        else:
-            print(f'Você não digitou uma opção válida.')
+            elif numero_pc == 2:
+                print(f' e o computador escolheu {lista[numero_pc]}. EMPATE!')
+                empates += 1
